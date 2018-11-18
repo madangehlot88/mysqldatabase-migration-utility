b16196b96d76f33cdcd75f215b8e95d7e6aa4fdb
madangehlot88 <madangehlot88@gmail.com>
2018-11-18 22:40:17 +0530

    .Net Core Utility to copy database from source to destination db. Initial Commit

---
+++ MySqlDataMigration/ClassConDestination.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+/// <summary>
+/// Destination Database Connection Class
+/// </summary>
+public class ClassConDestination
+{
+
+    public static string ConString = "";
+
+    MySqlConnection conn = new MySqlConnection(ConString);
+    public int ExecuteQuery(string query)
+    {
+        try
+        {
+            
+
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            else
+            {
+                conn.Close();
+                conn.Open();
+            }
+
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            int status = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            return status;
+        }
+        catch (Exception exc)
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+
+            return -1;
+
+        }
+
+
+    }
+
+    public int SelectScaler(string query)
+    {
+        try
+        {
+            conn = new MySqlConnection(ConString);
+
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            else
+            {
+                conn.Close();
+                conn.Open();
+            }
+
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            object result = cmd.ExecuteScalar();
+            conn.Close();
+            if (result != null)
+            {
+                return Convert.ToInt32(result);
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+        catch (Exception exc)
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+
+
+            return -1;
+        }
+
+
+    }
+
+    public object SelectScalerObj(string query)
+    {
+        try
+        {
+            conn = new MySqlConnection(ConString);
+
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            else
+            {
+                conn.Close();
+                conn.Open();
+            }
+
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            object result = cmd.ExecuteScalar();
+            conn.Close();
+            if (result != null)
+            {
+                return result;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+        catch (Exception exc)
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+
+
+            return -1;
+        }
+
+
+    }
+
+
+    public object SelectScalerBool(string query)
+    {
+        try
+        {
+            conn = new MySqlConnection(ConString);
+
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            else
+            {
+                conn.Close();
+                conn.Open();
+            }
+
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            object result = cmd.ExecuteScalar();
+            conn.Close();
+            if (result != null)
+            {
+                return result;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        catch (Exception exc)
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+
+
+            return false;
+        }
+
+
+    }
+
+
+    public DataSet SelectDS(string query)
+    {
+        DataSet ds = new DataSet();
+        try
+        {
+
+            conn = new MySqlConnection(ConString);
+
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            else
+            {
+                conn.Close();
+                conn.Open();
+            }
+
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+            adp.Fill(ds);
+            conn.Close();
+            return ds;
+
+        }
+        catch (Exception exc)
+        {
+            conn.Close();
+        }
+
+        return ds;
+    }
+
+
+    public DataTable SelectDT(string query)
+    {
+        DataTable dt = new DataTable();
+        try
+        {
+
+            conn = new MySqlConnection(ConString);
+
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            else
+            {
+                conn.Close();
+                conn.Open();
+            }
+
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+            adp.Fill(dt);
+            conn.Close();
+            return dt;
+
+        }
+        catch (Exception exc)
+        {
+            conn.Close();
+        }
+
+        return dt;
+    }
+
+
+  
+
+
+
+}
+
+
--- MySqlDataMigration/Program.cs
+++ MySqlDataMigration/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 
 namespace MySqlDataMigration
 {
+
+    /// <summary>
+    /// Program to truncate and copy data from source to destination. Make sure connection strings are using root user
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Init source database connection
+            ClassConSource conSource = new ClassConSource();
+
+            //Init destination database connection
+            ClassConDestination conDestination = new ClassConDestination();
+
+            //fetch all tables from source database
+            DataTable dtSource = conSource.SelectDT("SHOW TABLES");
+
+
+            //loop through all table (source database)
+            foreach(DataRow dataRow in dtSource.Rows)
+            {
+
+
+                //first truncate destination table
+                string truncateDestination = "truncate table " + dataRow[0] + "";
+                conDestination.ExecuteQuery(truncateDestination);
+
+                //fetch source table columns table
+
+                var columns = conSource.SelectScalerObj("SELECT GROUP_CONCAT(concat('`',column_name,'`')) FROM information_schema.columns WHERE table_schema = 'tefsalapp1' AND table_name = '" + dataRow[0] + "' GROUP BY table_schema,table_name");
+
+                //insert into destination table (which are availble in source only, for correct mapping). bulk copy rows from source table to destination table.
+                // make sure both databases have root access (given in connection string)
+                string insertDestination = "insert into " + dataRow[0] + "("+ columns + ") select * from tefsalapp1." + dataRow[0] + "";
+                conDestination.ExecuteQuery(insertDestination);
+
+
+            }
+
+
+
+
         }
     }
 }
